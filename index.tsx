5532af044d99a8a0b0a86665dac8667e36e6d033
LOURD JVKE <jvkechris@gmail.com>
2026-02-19 20:07:27 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,6 +2,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import '@fontsource/inter';
+import '@fontsource/playfair-display';
+import '@fontsource/montserrat';
+import '@fontsource/bangers';
+import '@fontsource/lobster';
+import '@fontsource/permanent-marker';
+import '@fontsource/sacramento';
+import '@fontsource/press-start-2p';
+import '@fontsource/monoton';
+import '@fontsource/alfa-slab-one';
+import '@fontsource/cinzel-decorative';
+import '@fontsource/faster-one';
+import '@fontsource/righteous';
+import '@fontsource/fredoka-one';
+import '@fontsource/orbitron';
+import '@fontsource/special-elite';
+import '@fontsource/cookie';
+import '@fontsource/satisfy';
+import '@fontsource/kaushan-script';
+import '@fontsource/pinyon-script';
+import '@fontsource/rochester';
+import '@fontsource/abril-fatface';
+import '@fontsource/comfortaa';
+import '@fontsource/unifrakturmaguntia';
+import '@fontsource/creepster';
+import '@fontsource/nosifer';
+import '@fontsource/bungee-shade';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
