317b5d707932d23bd7835873f144dfcd6a40f954
LOURD JVKE <jvkechris@gmail.com>
2026-02-22 18:43:52 +0100

    Update IconLibrary.tsx

--- components/IconLibrary.tsx
+++ components/IconLibrary.tsx
@@ -84,20 +84,13 @@ export const Icons = {
     </svg>
   ),
   Wand2: (props: React.SVGProps<SVGSVGElement>) => (
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-      <path d="m21.64 3.64-1.28-1.28a1.21 1.21 0 0 0-1.71 0L11.5 9.51l8.84 8.84 7.11-7.11a1.21 1.21 0 0 0 0-1.71l-1.28-1.28zM12 12l-2-2"></path>
-      <path d="M9 9 2.16 2.16a1.21 1.21 0 0 0-1.71 0L.29 2.32a1.21 1.21 0 0 0 0 1.71l8.13 8.13"></path>
-      <path d="M14 4 4 14"></path>
-      <path d="M15 11h.01"></path>
-      <path d="M11 15h.01"></path>
-      <path d="M6 20h.01"></path>
-      <path d="M20 6h.01"></path>
+    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" {...props}>
+        <path fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 12c4.5 0 9-4.5 9-9c0 4.5 4.5 9 9 9c-4.5 0-9 4.5-9 9c0-4.5-4.5-9-9-9m-1 7.5c.833 0 2.5-1.667 2.5-2.5c0 .833 1.667 2.5 2.5 2.5c-.833 0-2.5 1.667-2.5 2.5c0-.833-1.667-2.5-2.5-2.5M16 5c1 0 3-2 3-3c0 1 2 3 3 3c-1 0-3 2-3 3c0-1-2-3-3-3" color="currentColor"/>
     </svg>
   ),
   Sparkles: (props: React.SVGProps<SVGSVGElement>) => (
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-        <path d="m12 3-1.16 4.34-4.34 1.16 4.34 1.16L12 14l1.16-4.34 4.34-1.16-4.34-1.16L12 3z"/>
-        <path d="M3.5 15.5.5 16l3 3.5 1-3.5-3-1zm16-7L16.5.5l-1 3 3.5 3 1-3.5z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 24 24" {...props}> 
+        <path fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 12c4.5 0 9-4.5 9-9c0 4.5 4.5 9 9 9c-4.5 0-9 4.5-9 9c0-4.5-4.5-9-9-9m-1 7.5c.833 0 2.5-1.667 2.5-2.5c0 .833 1.667 2.5 2.5 2.5c-.833 0-2.5 1.667-2.5 2.5c0-.833-1.667-2.5-2.5-2.5M16 5c1 0 3-2 3-3c0 1 2 3 3 3c-1 0-3 2-3 3c0-1-2-3-3-3" color="currentColor"/>
     </svg>
   ),
   Undo2: (props: React.SVGProps<SVGSVGElement>) => (
